776b38057850fbc456f7d9bbd8b2524b0a8792a4
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2025-03-27 17:58:31 -0700

    Release AcousticMobilePushReactNative build: 3.9.39
    March 28 2025 AcousticMobilePushReactNative Release
    AcousticMobilePushReactNative 3.9.39

--- plugins/react-native-acoustic-mobile-push-inbox/README.md
+++ plugins/react-native-acoustic-mobile-push-inbox/README.md
@@ -1,21 +1,5 @@
-# react-native-acoustic-mobile-push-inbox
-Required Campaign plugin for inbox feature.
+# Acoustic Campaign - React-Native Plugin
 
-### Plugin Dependencies
-react-native-acoustic-mobile-push
-react-native-acoustic-mobile-push-inapp
-----
+Campaign plugin has been published to NPM for React-Native apps. Add the plugin with 'yarn' or 'npm' to start configuring basic or advanced mobile app messaging features using an unified configuration file called 'CampaignConfig.json' for both iOS and Android apps.
 
-[Overview](https://developer.goacoustic.com/acoustic-campaign/docs/add-the-react-native-plug-in-to-your-app#overview)
----
-
-## Install
-Add `react-native-acoustic-mobile-push-inbox` to the project.
-
-```shell yarn
-yarn add react-native-acoustic-mobile-push-inbox
-```
-
-```shell npm
-npm i react-native-acoustic-mobile-push-inbox
-```
+Please review base plugin at https://www.npmjs.com/package/react-native-acoustic-mobile-push for instructions.
--- plugins/react-native-acoustic-mobile-push/android/src/main/java/co/acoustic/mobile/push/RNAcousticMobilePushBroadcastReceiver.java
+++ plugins/react-native-acoustic-mobile-push/android/src/main/java/co/acoustic/mobile/push/RNAcousticMobilePushBroadcastReceiver.java
@@ -7,17 +7,17 @@
  * Acoustic, L.P. Any unauthorized copying or distribution of content from this file is
  * prohibited.
  */
- 
+
 package co.acoustic.mobile.push;
 
 import android.content.Intent;
 import android.content.Context;
 import android.os.Bundle;
 import android.location.Location;
+import android.text.TextUtils;
+import android.util.Log;
 
 import co.acoustic.mobile.push.sdk.api.MceBroadcastReceiver;
-import co.acoustic.mobile.push.sdk.api.MceSdk;
-import co.acoustic.mobile.push.sdk.apiinternal.MceSdkInternal;
 import co.acoustic.mobile.push.sdk.location.MceLocation;
 import co.acoustic.mobile.push.sdk.api.notification.NotificationDetails;
 import co.acoustic.mobile.push.sdk.api.attribute.AttributesOperation;
@@ -35,166 +35,184 @@ import com.facebook.react.bridge.WritableNativeMap;
 import org.json.JSONException;
 
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Date;
-import java.text.SimpleDateFormat;  
+import java.text.SimpleDateFormat;
 
 public class RNAcousticMobilePushBroadcastReceiver extends MceBroadcastReceiver {
 
-    private static final String TAG = "RNAcousticMobilePushBroadcastReceiver";
+  private static final String TAG = "RNAcousticMobilePushBroadcastReceiver";
 
-    @Override
-    public void onLocationUpdate(Context context, Location location) {
-        RNAcousticMobilePushModule.sendEvent("DownloadedLocations", null);
+  @Override
+  public void onLocationUpdate(Context context, Location location) {
+    RNAcousticMobilePushModule.sendEvent("DownloadedLocations", null);
+  }
+
+  @Override
+  public void onActionNotYetRegistered(Context context, String actionType) {
+    Logger.d(TAG, "Sending CustomPushNotYetRegistered event to Javascript");
+    WritableNativeMap map = new WritableNativeMap();
+    map.putString("type", actionType);
+    RNAcousticMobilePushModule.sendEvent("CustomPushNotYetRegistered", map);
+  }
+
+  @Override
+  public void onActionNotRegistered(Context context, String actionType) {
+    Logger.d(TAG, "Sending CustomPushNotRegistered event to Javascript");
+    WritableNativeMap map = new WritableNativeMap();
+    map.putString("type", actionType);
+    RNAcousticMobilePushModule.sendEvent("CustomPushNotRegistered", map);
+  }
+
+  @Override
+  public void onSdkRegistered(Context context) {
+    try {
+      RNAcousticMobilePushModule.sendEvent("Registered", null);
+      RNAcousticMobilePushModule.sendReactNativeChannelAttribute();
+    } catch (Exception e) {
+      Log.e(TAG, "Error sending RegistrationChanged event: " + e.getMessage());
     }
+  }
 
-    @Override
-    public void onActionNotYetRegistered(Context context, String actionType) {
-        Logger.d(TAG, "Sending CustomPushNotYetRegistered event to Javascript");
-        WritableNativeMap map = new WritableNativeMap();
-        map.putString("type", actionType);
-        RNAcousticMobilePushModule.sendEvent("CustomPushNotYetRegistered", map);
+  @Override
+  public void onSdkRegistrationChanged(Context context) {
+    try {
+      RNAcousticMobilePushModule.sendEvent("RegistrationChanged", null);
+      RNAcousticMobilePushModule.sendReactNativeChannelAttribute();
+    } catch (Exception e) {
+      Log.e(TAG, "Error sending RegistrationChanged event: " + e.getMessage());
     }
+  }
 
-    @Override
-    public void onActionNotRegistered(Context context, String actionType) {
-        Logger.d(TAG, "Sending CustomPushNotRegistered event to Javascript");
-        WritableNativeMap map = new WritableNativeMap();
-        map.putString("type", actionType);
-        RNAcousticMobilePushModule.sendEvent("CustomPushNotRegistered", map);
+  @Override
+  public void onSdkRegistrationUpdated(Context context) {
+    try {
+      RNAcousticMobilePushModule.sendEvent("RegistrationChanged", null);
+      RNAcousticMobilePushModule.sendReactNativeChannelAttribute();
+    } catch (Exception e) {
+      Log.e(TAG, "Error sending RegistrationChanged event: " + e.getMessage());
     }
+  }
 
-    @Override
-    public void onSdkRegistered(Context context) {
-        RNAcousticMobilePushModule.sendEvent("Registered", null);
-        RNAcousticMobilePushModule.sendReactNativeChannelAttribute();
+  @Override
+  public void onMessage(Context context, NotificationDetails notificationDetails, Bundle bundle) {
+  }
+
+  @Override
+  public void onMessagingServiceRegistered(Context context) {
+  }
+
+  @Override
+  public void onC2dmError(Context context, String error) {
+  }
+
+  @Override
+  public void onSessionStart(Context context, Date date) {
+  }
+
+  @Override
+  public void onSessionEnd(Context context, Date date, long l) {
+  }
+
+  @Override
+  public void onNotificationAction(Context context, Date date, String type, String name, String value) {
+  }
+
+  @Override
+  public void onAttributesOperation(Context context, AttributesOperation attributesOperation) {
+    if (attributesOperation.getType() == AttributesOperation.Type.updateAttributes) {
+      List<Attribute> attributes = attributesOperation.getAttributes();
+      WritableNativeMap attributesMap = translateAttributes(attributes);
+      WritableNativeMap map = new WritableNativeMap();
+      map.putMap("attributes", attributesMap);
+      RNAcousticMobilePushModule.sendEvent("UpdateUserAttributesSuccess", map);
+    } else if (attributesOperation.getType() == AttributesOperation.Type.deleteAttributes) {
+      List<String> keys = attributesOperation.getAttributeKeys();
+      WritableNativeArray keyArray = new WritableNativeArray();
+      Iterator keyIterator = keys.iterator();
+      while (keyIterator.hasNext()) {
+        String key = (String) keyIterator.next();
+        keyArray.pushString(key);
+      }
+
+      WritableNativeMap map = new WritableNativeMap();
+      map.putArray("keys", keyArray);
+      RNAcousticMobilePushModule.sendEvent("DeleteUserAttributesSuccess", map);
     }
+  }
 
-    @Override
-    public void onSdkRegistrationChanged(Context context) {
-        RNAcousticMobilePushModule.sendEvent("RegistrationChanged", null);
-        RNAcousticMobilePushModule.sendReactNativeChannelAttribute();
-    }
+  WritableNativeMap translateAttributes(List<Attribute> attributes) {
+    WritableNativeMap attributesMap = new WritableNativeMap();
 
-    @Override
-    public void onSdkRegistrationUpdated(Context context) {
-        RNAcousticMobilePushModule.sendEvent("RegistrationChanged", null);
-        RNAcousticMobilePushModule.sendReactNativeChannelAttribute();
-    }
-
-    @Override
-    public void onMessage(Context context, NotificationDetails notificationDetails, Bundle bundle) {
-    }
-
-    @Override
-    public void onMessagingServiceRegistered(Context context) {
-    }
-
-    @Override
-    public void onC2dmError(Context context, String error) {
-    }
-
-    @Override
-    public void onSessionStart(Context context, Date date) {
-    }
-
-    @Override
-    public void onSessionEnd(Context context, Date date, long l) {
-    }
-
-    @Override
-    public void onNotificationAction(Context context, Date date, String type, String name, String value) {
-    }
-
-    @Override
-    public void onAttributesOperation(Context context, AttributesOperation attributesOperation) {
-        if(attributesOperation.getType() == AttributesOperation.Type.updateAttributes) {
-            List<Attribute> attributes = attributesOperation.getAttributes();
-            WritableNativeMap attributesMap = translateAttributes(attributes);
-            WritableNativeMap map = new WritableNativeMap();
-            map.putMap("attributes", attributesMap);
-            RNAcousticMobilePushModule.sendEvent("UpdateUserAttributesSuccess", map);
-        } else if(attributesOperation.getType() == AttributesOperation.Type.deleteAttributes) {
-            List<String> keys = attributesOperation.getAttributeKeys();
-            WritableNativeArray keyArray = new WritableNativeArray();
-            Iterator keyIterator = keys.iterator();
-            while(keyIterator.hasNext()) {
-                String key = (String) keyIterator.next();
-                keyArray.pushString(key);
-            }
-
-            WritableNativeMap map = new WritableNativeMap();
-            map.putArray("keys", keyArray);
-            RNAcousticMobilePushModule.sendEvent("DeleteUserAttributesSuccess", map);
+    try {
+      Iterator attributeIterator = attributes.iterator();
+      while (attributeIterator.hasNext()) {
+        Attribute attribute = (Attribute) attributeIterator.next();
+        if (attribute instanceof StringAttribute) {
+          attributesMap.putString(attribute.getKey(), (String) attribute.getValue());
+        } else if (attribute instanceof NumberAttribute) {
+          Double doubleValue = (Double) attribute.getValue();
+          attributesMap.putDouble(attribute.getKey(), doubleValue.doubleValue());
+        } else if (attribute instanceof DateAttribute) {
+          if (attribute.getValue() != null && attribute.getValue() instanceof Date) {
+            Date value = (Date) attribute.getValue();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSS'Z'");
+            attributesMap.putString(attribute.getKey(), dateFormat.format(value));
+          }
+        } else if (attribute instanceof BooleanAttribute) {
+          Boolean booleanValue = (Boolean) attribute.getValue();
+          attributesMap.putBoolean(attribute.getKey(), booleanValue.booleanValue());
         }
+      }
+    } catch (Exception e) {
+      Log.e(TAG, "Error translating attributes", e);
+    }
+    return attributesMap;
+  }
+
+  @Override
+  public void onEventsSend(Context context, List<Event> events) {
+    WritableNativeMap map = new WritableNativeMap();
+    WritableNativeArray eventArray = new WritableNativeArray();
+
+    Iterator eventIterator = events.iterator();
+    while (eventIterator.hasNext()) {
+      Event event = (Event) eventIterator.next();
+
+      WritableNativeMap eventMap = new WritableNativeMap();
+      eventMap.putString("name", event.getName());
+      eventMap.putString("type", event.getType());
+      eventMap.putDouble("timestamp", event.getTimestamp().getTime());
+
+      List<Attribute> attributes = event.getAttributes();
+      WritableNativeMap attributesMap = translateAttributes(attributes);
+      eventMap.putMap("attributes", attributesMap);
+
+      eventMap.putString("attribution", event.getAttribution());
+      eventMap.putString("mailingId", event.getMailingId());
+
+      eventArray.pushMap(eventMap);
     }
 
-    WritableNativeMap translateAttributes(List<Attribute> attributes) {
-        WritableNativeMap attributesMap = new WritableNativeMap();
-        Iterator attributeIterator = attributes.iterator();
-        while(attributeIterator.hasNext()) {
-            Attribute attribute = (Attribute) attributeIterator.next();
-            if(attribute instanceof StringAttribute) {
-                attributesMap.putString(attribute.getKey(), (String)attribute.getValue());
-            } else if(attribute instanceof NumberAttribute) {
-                Double doubleValue = (Double)attribute.getValue();
-                attributesMap.putDouble(attribute.getKey(), doubleValue.doubleValue());
-            } else if(attribute instanceof DateAttribute) {
-                Date value = (Date) attribute.getValue();
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSS'Z'");
-                attributesMap.putString(attribute.getKey(), dateFormat.format(value));
-            } else if(attribute instanceof BooleanAttribute) {
-                Boolean booleanValue = (Boolean)attribute.getValue();
-                attributesMap.putBoolean(attribute.getKey(), booleanValue.booleanValue() );
-            }
-        }
-        return attributesMap;
-    }
+    map.putArray("events", eventArray);
+    RNAcousticMobilePushModule.sendEvent("EventSuccess", map);
+  }
 
-    @Override
-    public void onEventsSend(Context context, List<Event> events) {
-        WritableNativeMap map = new WritableNativeMap();
-        WritableNativeArray eventArray = new WritableNativeArray();
+  @Override
+  public void onIllegalNotification(Context context, Intent intent) {
+  }
 
-        Iterator eventIterator = events.iterator();
-        while(eventIterator.hasNext()) {
-            Event event = (Event) eventIterator.next();
+  @Override
+  public void onNonMceBroadcast(Context context, Intent intent) {
+  }
 
-            WritableNativeMap eventMap = new WritableNativeMap();
-            eventMap.putString("name", event.getName());
-            eventMap.putString("type", event.getType());
-            eventMap.putDouble("timestamp", event.getTimestamp().getTime());
+  @Override
+  public void onLocationEvent(Context context, MceLocation location, LocationType locationType, LocationEventType locationEventType) {
+  }
 
-            List<Attribute> attributes = event.getAttributes();
-            WritableNativeMap attributesMap = translateAttributes(attributes);
-            eventMap.putMap("attributes", attributesMap);
-
-            eventMap.putString("attribution", event.getAttribution());
-            eventMap.putString("mailingId", event.getMailingId());
-
-            eventArray.pushMap(eventMap);
-        }
-
-        map.putArray("events", eventArray);
-        RNAcousticMobilePushModule.sendEvent("EventSuccess", map);
-    }
-
-    @Override
-    public void onIllegalNotification(Context context, Intent intent) {
-    }
-
-    @Override
-    public void onNonMceBroadcast(Context context, Intent intent) {
-    }
-
-    @Override
-    public void onLocationEvent(Context context, MceLocation location, LocationType locationType, LocationEventType locationEventType) {
-    }
-
-    @Override
-    public void onInboxCountUpdate(Context context) {
-        RNAcousticMobilePushModule.sendEvent("InboxCountUpdate", null);
-    }
+  @Override
+  public void onInboxCountUpdate(Context context) {
+    RNAcousticMobilePushModule.sendEvent("InboxCountUpdate", null);
+  }
 
 }
